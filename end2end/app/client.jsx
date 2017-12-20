99b79450db27b33c5e1d7a3318de129e9637ad11
Zhang, Mingda(mingdzhang) <mingdzhang@paypal.com>
2017-12-20 15:48:29 -0800

    imtegrate redux

--- end2end/app/client.jsx
+++ end2end/app/client.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './universal/reducer' 
 import RGB from './universal/RGB'
 import './app.scss'
 
+const store = createStore(reducer)
+
 render(
-	<RGB/>,
+    <Provider store={store}>
+	    <RGB />
+    </Provider>,
 	document.getElementById('app')
 )
--- end2end/app/universal/RGB.jsx
+++ end2end/app/universal/RGB.jsx
@@ -1,20 +1,14 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import * as actions from './actions'
 import ColorBlock from './ColorBlock'
 import Slider from './Slider'
 
-export default class RGB extends Component {
-
-	state = {
-		r: 244,
-		g: 158,
-		b: 66
-	}
-
-
+export class RGB extends Component {
 	render() {
 
-		const {r,g,b} = this.state
-		const update = color =>val => this.setState({ [color]:val })
+		const {r,g,b, updateColor} = this.props
+		const update = color =>val => updateColor(color, val)
 		return (
 			<center>
 				<ColorBlock r={r} g={g} b={b}/>
@@ -25,3 +19,12 @@ export default class RGB extends Component {
 			)
 	}
 }
+
+export default connect(
+    state => ({
+        r: state.r,
+        g: state.g,
+        b: state.b
+    }),
+    { updateColor: actions.updateColor}
+)(RGB)
